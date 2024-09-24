c4a3fe05a1ac17eb930e4431676688deee26c614
tjnut3 <notakcoblet@gmail.com>
2024-09-24 14:37:37 +0700

    bug fix

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 statusCheck = false;
 
 function Showsharebar() {
+    let replyimg = document.getElementById("replyimg");
+    let replyBtn = document.getElementById("replyBtn");
     let sharecard = document.getElementById("sharecard");
     let sharebox = document.getElementById("sharebox");
     let profile = document.getElementById("profile");
@@ -9,20 +11,25 @@ function Showsharebar() {
         if (statusCheck == true) {
             sharecard.style.display = "flex";
             profile.style.display = "none";
-        
         }
         else {                                          
             sharecard.style.display = "none";
             profile.style.display = "flex";
+            replyBtn.style.backgroundColor = "#ecf2f8"
+            replyimg.style.filter = "none";
         }
     }
     if (window.innerWidth >= 768) {
         if (statusCheck == true) {
+            replyBtn.style.backgroundColor = "#48556a"
+            replyimg.style.filter = "grayscale(100%) brightness(1000%)";
             sharebox.style.display = "flex";
             profile.style.display = "flex";
         }
         else {
             sharebox.style.display = "none";
+            replyBtn.style.backgroundColor = "#ecf2f8"
+            replyimg.style.filter = "none";
         }
     }
 }
@@ -34,6 +41,8 @@ window.addEventListener("resize", function() {
             profile.style.display = "flex";
             sharebox.style.display = "flex";
             sharecard.style.display = "none";
+            replyBtn.style.backgroundColor = "#48556a"
+            replyimg.style.filter = "grayscale(100%) brightness(1000%)";
         }
     }
     else {
